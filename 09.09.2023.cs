8aec6caf61e123eb58a9cfbb7791ef3cabb623c2
TamerlanAlliyev <146847336+TamerlanAlliyev@users.noreply.github.com>
2023-10-09 23:13:48 -0700

    09.09.2023 task

---
+++ 09.09.2023.cs
@@ -0,0 +1,17 @@
+﻿//1.int tipindən array və 1 int tipindən ədəd qəbul edən metod yazın. Metod bu ədədin bu arrayin hər hansı bir elementinə bərabər olub-olmadığını tapsın.
+
+int[] myArray = { 1, 2, 3, 4, 5 };
+int myNumber = 3;
+static bool Does(int[] array, int num)
+{
+    foreach (int element in array)
+    {
+        if (element == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+Console.WriteLine("Array daxilində ədəd tapıldı: true");
+Console.WriteLine("Array daxilində ədəd tapılmadı: false");
