637d18efc142487f9e3b55ac3a835703e52f16ee
TamerlanAlliyev <146847336+TamerlanAlliyev@users.noreply.github.com>
2023-10-10 22:38:33 -0700

    Substring metodu custom

---
+++ Substring metodu.cs
@@ -0,0 +1,10 @@
+﻿string[] Name = { "HelloP237group" };
+int say = 1;
+foreach (var name in Name)
+{
+    for (int i = 0; i < name.Length; i++)
+    {
+        Console.WriteLine(say);
+        say++;
+    }
+}
